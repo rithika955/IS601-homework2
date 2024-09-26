40814eb7cff4f31f36b3af161fe5ea4082ec5d35
rithika955 <rk955@njit.edu>
2024-09-26 12:19:55 -0400

    fixes

--- projectsetup/calculator/__init__.py
+++
@@ -1,7 +0,0 @@
-''' init function'''
-def add(a,b):
-    '''addition function'''
-    return a+b
-def subtract(a,b):
-    '''subtraction function'''
-    return a-b
--- projectsetup/tests/test_calculator.py
+++
@@ -1,9 +0,0 @@
-'''My Calculator Test'''
-from calculator import add,subtract
-
-def test_addition():
-    '''Addition function'''
-    assert add(2,2) == 4
-def test_sutraction():
-    '''Subtraction Function'''
-    assert subtract(2,2) == 0
